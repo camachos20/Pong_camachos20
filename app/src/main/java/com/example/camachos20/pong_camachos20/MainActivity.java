2eca3abf97d69cad7a617e336506d28528447008
camachos20 <camachos20@up.edu>
2018-03-21 13:53:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/camachos20/pong_camachos20/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.camachos20.pong_camachos20;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.LinearLayout;
+
+/**
+ * PongMainActivity
+ *
+ * This is the activity for the Pong game. It attaches a PongAnimator to
+ * an AnimationSurface.
+ *
+ * @author Andrew Nuxoll
+ * @author Steven R. Vegdahl
+ * @version July 2013
+ *
+ */
+public class MainActivity extends Activity {
+
+    /**
+     * creates an AnimationSurface containing a TestAnimator.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Connect the animation surface with the animator
+        AnimationSurface mySurface = (AnimationSurface) this
+                .findViewById(R.id.animationSurface);
+        mySurface.setAnimator(new BallAnimator());
+    }
+}
+
+
+
+
