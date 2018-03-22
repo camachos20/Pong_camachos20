a293f4b3dca41aaf1c6bb6074466ee5f29a1757f
camachos20 <camachos20@up.edu>
2018-03-21 18:10:14 -0700

    still working

--- app/src/main/java/com/example/camachos20/pong_camachos20/BallAnimator.java
+++ app/src/main/java/com/example/camachos20/pong_camachos20/BallAnimator.java
@@ -3,20 +3,31 @@ package com.example.camachos20.pong_camachos20;
 import android.graphics.*;
 import android.view.MotionEvent;
 
+import java.util.Random;
+
+import static java.lang.Boolean.FALSE;
+
 
 /**
- * class that animates a ball repeatedly moving diagonally on
- * simple background
+ * class that animates a ball to shoot out randomly and bounce off the 3 walls or exit the screen
  *
- * @author Steve Vegdahl
- * @author Andrew Nuxoll
- * @version February 2016
+ * @author Stephanie Camacho
+ * @version 21 March 2018
  */
+
 public class BallAnimator implements Animator {
 
     // instance variables
-    private int count = 0; // counts the number of logical clock ticks
-    private boolean goBackwards = false; // whether clock is ticking backwards
+    private int countX; //x position of ball
+    private int countY; //y position of ball
+    private boolean reverseX = false;
+    private boolean reverseY = false;
+    private Random gen = new Random();
+
+    private int screenWidth = 2048;
+    private int screenLength = 1536;
+
+
 
     /**
      * Interval between animation frames: .03 seconds (i.e., about 33 times
@@ -29,24 +40,15 @@ public class BallAnimator implements Animator {
     }
 
     /**
-     * The background color: a light blue.
+     * The background color: black.
      *
      * @return the background color onto which we will draw the image.
      */
     public int backgroundColor() {
-        // create/return the background color
-        return Color.rgb(180, 200, 255);
+        // return the background color
+        return Color.BLACK;
     }
 
-    /**
-     * Tells the animation whether to go backwards.
-     *
-     * @param b true iff animation is to go backwards.
-     */
-    public void goBackwards(boolean b) {
-        // set our instance variable
-        goBackwards = b;
-    }
 
     /**
      * Action to perform on clock tick
@@ -55,27 +57,56 @@ public class BallAnimator implements Animator {
      */
     public void tick(Canvas g) {
         // bump our count either up or down by one, depending on whether
-        // we are in "backwards mode".
-        if (goBackwards) {
-            count--;
+        // we are in "reverse mode"
+        if (reverseX) {
+            countX--;
         }
         else {
-            count++;
+            countX++;
         }
+        if(reverseY) {
+            countY--;
+        }
+        else {
+            countY++;
+        }
+
 
         // Determine the pixel position of our ball.  Multiplying by 15
         // has the effect of moving 15 pixel per frame.  Modding by 600
         // (with the appropriate correction if the value was negative)
         // has the effect of "wrapping around" when we get to either end
         // (since our canvas size is 600 in each dimension).
-        int num = (count*15)%600;
-        if (num < 0) num += 600;
+        int numX = (countX*15)%600;
+        if (numX < 0) numX += 600;
+
+        int numY = (countY*15)%600;
+        if (numY < 0) numY+=600;
+
+        //check where ball is and determine whether it hits a wall and reverse the direction
+        if (numX < 0 || numX > screenWidth)
+        {
+            reverseX = !reverseX;
+        }
+        if (numY < 0 || numY > screenLength)
+        {
+            reverseY = !reverseY;
+        }
+
+
+
 
         // Draw the ball in the correct position.
         Paint redPaint = new Paint();
         redPaint.setColor(Color.RED);
-        g.drawCircle(num, num, 60, redPaint);
+        g.drawCircle(numX, numY, 40, redPaint);
         redPaint.setColor(0xff0000ff);
+
+        //Draw paddle at bottom of screen
+        Paint whitePaint = new Paint();
+        whitePaint.setColor(Color.WHITE);
+        g.drawRect(screenLength, 874 ,screenLength-20,1174, whitePaint);
+
     }
 
     /**
@@ -99,12 +130,7 @@ public class BallAnimator implements Animator {
     /**
      * reverse the ball's direction when the screen is tapped
      */
-    public void onTouch(MotionEvent event)
-    {
-        if (event.getAction() == MotionEvent.ACTION_DOWN)
-        {
-            goBackwards = !goBackwards;
-        }
+    public void onTouch(MotionEvent event) {
     }
 
 
